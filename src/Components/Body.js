deda00da2c551895d1516a64bec8118409d35152
kramstyles <mark.eke@decagon.dev>
2022-01-29 21:22:38 +0100

    worked on the notes

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -7,11 +7,12 @@ function Body() {
             <div className="content">
                 <div style = {{textAlign: 'center'}}>
                     <h1>Just Michael</h1>
-                    <p style={{ color: 'dodgerblue', fontWeight: 'bold', fontSize: '90%', margin: "8px 0" }}>YOUR PYTHON GUY!</p>
+                    <p style={{ color: 'dodgerblue', fontWeight: 'bold', fontSize: '70%', margin: "5px 0 0" }}>YOUR PYTHON GUY!</p>
                     <small><a href="mailto:kramstyles@outlook.com">Official: kramstyles@outlook.com</a></small>
                 </div>
                 <div className="links">
-                    
+                    <a href="mailto:kramstyles@outlook.com"><i className="fa fa-envelope"> </i> Email</a>
+                    <a href="https://linkedin.com/eke-mark"><i className="fab fa-linkedin"> </i> LinkedIn</a>
                 </div>
             </div>
         </div>
