257668825dd12d7836c228755831d71e09f34425
kramstyles <mark.eke@decagon.dev>
2022-01-29 21:10:32 +0100

    worked on the name

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,11 +1,18 @@
 import React from "react"
 
-function Body(){
+function Body() {
     return (
         <div>
             <div className="header_img"></div>
             <div className="content">
-                
+                <div style = {{textAlign: 'center'}}>
+                    <h1>Just Michael</h1>
+                    <p style={{ color: 'dodgerblue', fontWeight: 'bold', fontSize: '90%', margin: "8px 0" }}>YOUR PYTHON GUY!</p>
+                    <small><a href="mailto:kramstyles@outlook.com">Official: kramstyles@outlook.com</a></small>
+                </div>
+                <div className="links">
+                    
+                </div>
             </div>
         </div>
     )
