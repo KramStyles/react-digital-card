e4a6e6953eb201052d7b0b863d5763b4a05d2bfd
kramstyles <mark.eke@decagon.dev>
2022-01-29 20:57:36 +0100

    worked on the footer

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -4,6 +4,9 @@ function Body(){
     return (
         <div>
             <div className="header_img"></div>
+            <div className="content">
+                
+            </div>
         </div>
     )
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,12 @@ import React from "react"
 
 function Footer(){
     return (
-        <footer>Hello</footer>
+        <footer>
+            <a href="https://twitter.com/kramstyles"><i className="fab fa-twitter"></i></a>
+            <a href="https://facebook.com/kramstyles"><i className="fab fa-facebook"></i></a>
+            <a href="https://github.com/kramstyles"><i className="fab fa-github"></i></a>
+            <a href="https://linkedin.com/in/eke-mark"><i className="fab fa-linkedin"></i></a>
+        </footer>
     )
 }
 
