eef35ba5aa11d580ffa1e3ee257a565498b5bc4c
kramstyles <mark.eke@decagon.dev>
2022-05-09 01:06:26 +0100

    added rel='noreferrer' for netlify

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,10 +3,10 @@ import React from "react"
 function Footer(){
     return (
         <footer>
-            <a href="https://twitter.com/kramstyles" target={'_blank'}><i className="fab fa-twitter"></i></a>
-            <a href="https://facebook.com/kramstyles" target={'_blank'}><i className="fab fa-facebook"></i></a>
-            <a href="https://github.com/kramstyles" target={'_blank'}><i className="fab fa-github"></i></a>
-            <a href="https://linkedin.com/in/eke-mark" target={'_blank'}><i className="fab fa-linkedin"></i></a>
+            <a href="https://twitter.com/kramstyles" target={'_blank'} rel="noreferrer"><i className="fab fa-twitter"></i></a>
+            <a href="https://facebook.com/kramstyles" target={'_blank'} rel="noreferrer"><i className="fab fa-facebook"></i></a>
+            <a href="https://github.com/kramstyles" target={'_blank'} rel="noreferrer"><i className="fab fa-github"></i></a>
+            <a href="https://linkedin.com/in/eke-mark" target={'_blank'} rel="noreferrer"><i className="fab fa-linkedin"></i></a>
         </footer>
     )
 }
